319d5d4c2759a35a50db74fb0dbcf29355d711d3
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-29 23:42:33 +0900

    useState로 값 핸들링하기

--- src/components/Forminput.js
+++ src/components/Forminput.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forminput.css";
 
 //이름, 가격, 유형, 구입날자, 메모, 재구매의사
 const Forminput = () => {
+  const [itemState, setItemState] = useState({
+    name: "",
+    price: 0,
+    itemType: "",
+    purchaseDay: null,
+    memo: "",
+    reOrder: true,
+  });
+
+  const nameChangeHandler = (e) => {
+    setItemState((prevState) => ({ ...prevState, name: e.target.value }));
+    console.log(itemState.name);
+  };
+
+  const priceChangeHandler = (e) => {
+    setItemState((prevState) => ({ ...prevState, price: e.target.value }));
+    console.log(itemState.price);
+  };
+
+  const typeChangeHandler = (e) => {
+    setItemState((prevState) => ({ ...prevState, itemType: e.target.value }));
+  };
+
   return (
     <form className="form-input-box">
       <div className="box">
         <label for="name">이름: </label>
-        <input type="text" required />
+        <input
+          type="text"
+          value={itemState.name}
+          onChange={nameChangeHandler}
+          required
+        />
       </div>
       <div className="box">
         <label for="price">가격: </label>
-        <input type="number" required />
+        <input
+          type="number"
+          value={itemState.price}
+          onChange={priceChangeHandler}
+          required
+        />
       </div>
       <div className="box">
         <label for="name">유형: </label>
-        <select name="food-type">
-          <option value="food">food</option>
-          <option value="food">food</option>
-          <option value="food">food</option>
-          <option value="food">food</option>
-          <option value="food">food</option>
+        <select
+          name="food-type"
+          value={itemState.itemType}
+          onChange={typeChangeHandler}
+        >
+          <option value="aaa">a</option>
+          <option value="bbb">b</option>
+          <option value="ccc">c</option>
+          <option value="ddd">d</option>
+          <option value="eee">e</option>
         </select>
       </div>
       <div className="box">
